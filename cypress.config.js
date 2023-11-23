0c4bf1ea7d44d0073d93c46449ac3c8c526b1cfb
jaaah6 <tzkyhs16@gmail.com>
2023-11-23 12:02:57 +0700

    cypress test 002

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
+    env: {
+      lockedUser: 'locked_out_user'
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
+    defaultCommandTimeout: 5500,
+    screenshotOnRunFailure: false,
+    chromeWebSecurity: false
   },
 });
